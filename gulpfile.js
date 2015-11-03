6e8c60b4ee7749007b602e1ac0af52a51c4d873c
Nguyễn Minh Tiến <nguyenminhtiend@gmail.com>
2015-11-03 23:44:44 +0700

    first config

---
+++ gulpfile.js
@@ -0,0 +1,74 @@
+"use strict";
+
+var gulp = require('gulp');
+var connect = require('gulp-connect');
+var open = require('gulp-open');
+var browserify = require('browserify');
+var reactify = require('reactify');
+var source = require('vinyl-source-stream');
+var concat = require('gulp-concat');
+var jshint = require('gulp-jshint');
+
+var config = {
+	port: 8888,
+	devBaseUrl: 'http://localhost',
+	paths: {
+		html: './src/*.html',
+		js: './src/**/*.js',
+		css: [
+			'node_modules/bootstrap/dist/css/bootstrap.min.css',
+			'node_modules/bootstrap/dist/css/bootstrap-theme.min.css'	
+		],
+		mainJs: './src/main.js',
+		dist: './dist'
+	}
+};
+
+gulp.task('connect', function(){
+	connect.server({
+		root: ['dist'],
+		port: config.port,
+		base: config.devBaseUrl,
+		livereload: true
+	});
+});
+
+gulp.task('open', ['connect'], function(){
+	gulp.src('dist/index.html')
+		.pipe(open({ uri: config.devBaseUrl + ':' + config.port + '/'}));
+});
+
+gulp.task('html', function(){
+	gulp.src(config.paths.html)
+		.pipe(gulp.dest(config.paths.dist))
+		.pipe(connect.reload());
+});
+
+gulp.task('js', function(){
+	browserify(config.paths.mainJs)
+		.transform(reactify)
+		.bundle()
+		.on('error', console.error.bind(console))
+		.pipe(source('bundle.js'))
+		.pipe(gulp.dest(config.paths.dist + '/scripts'))
+		.pipe(connect.reload());
+});
+
+gulp.task('css', function(){
+	gulp.src(config.paths.css)
+		.pipe(concat('bundle.css'))
+		.pipe(gulp.dest(config.paths.dist + '/css'));
+});
+
+gulp.task('lint', function() {
+    return gulp.src(config.paths.js)
+        .pipe(jshint())
+        .pipe(jshint.reporter('default'));
+});
+
+gulp.task('watch', function(){
+	gulp.watch(config.paths.html, ['html']);
+	gulp.watch(config.paths.js, ['js']);
+});
+
+gulp.task('default', ['html', 'js', 'css', 'lint','open', 'watch']);
---
+++ src/components/homePage.jsx
@@ -0,0 +1,13 @@
+"use strict";
+
+var React = require('react');
+
+var Home = React.createClass({
+	render: function(){
+		return (
+			<div className="jumbotron">
+				<h1>Home page</h1>
+			</div>
+		)
+	}
+});
