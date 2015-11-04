d064cede80cc3dbf15694df7648680fd0fae14e1
nguyenminhtiend <nguyenminhtiend@gmail.com>
2015-11-04 15:20:34 +0700

    add route

---
+++ src/components/about/aboutPage.jsx
@@ -0,0 +1,15 @@
+"use strict";
+
+var React = require('react');
+
+var About = React.createClass({
+	render: function(){
+		return (
+			<h1>
+				This is about page
+			</h1>
+		)
+	}
+});
+
+module.exports = About;
---
+++ src/components/common/header.jsx
@@ -0,0 +1,15 @@
+"use strict";
+
+var React = require('react');
+
+var Header = React.createClass({
+	render: function(){
+		return (
+			<h1>
+				This is Header
+			</h1>
+		)
+	}
+});
+
+module.exports = Header;
